e70b260056dab330965fe8e5653012a363c1e36a
dbaigara <dbaigara@cern.ch>
2023-04-10 15:04:28 +0300

    homework 05 is done

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// 1. Таблицу с `id="age-table"`.
+// 2. Все элементы `label` внутри этой таблицы (их три).
+// 3. Первый `td` в этой таблице (со словом «Age»).
+// 4. Форму `form` с именем `name="search"`.
+// 5. Первый `input` в этой форме.
+// 6. Последний `input` в этой форме.
+
+// console.log(document.getElementById("age-table"))
+// console.log(document.getElementsByTagName("label"))
+// console.log(document.getElementsByTagName("td")[0])
+// document.querySelector('form[name="search"]')
+
+// let form = document.getElementsByName('search')[0]
+// form.getElementsByTagName('input')[0]
+
+// form.getElementsByTagName('input')[form.length - 1]
+
+// ---------------------------------------------------
+
+
+
+
